9fd3fdeef0dccf6bc179173cdda06c5205a7b894
Aziz H <aziz@autonomic.ai>
2020-11-29 18:09:29 -0500

    Add server reflection * Update build gradle with server reflection dependency * Update GreetingServer to add Reflection service

--- src/main/java/learn/masterclass/grpc/greeting/server/GreetingServer.java
+++ src/main/java/learn/masterclass/grpc/greeting/server/GreetingServer.java
@@ -2,14 +2,17 @@ package learn.masterclass.grpc.greeting.server;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.protobuf.services.ProtoReflectionService;
 
 public class GreetingServer {
 
   public static void main(String[] args) throws Exception {
     System.out.println("Hello Grpc Server");
 
-    Server server = ServerBuilder.forPort(50051)
+    Server server;
+    server = ServerBuilder.forPort(50051)
         .addService(new GreetServiceImpl())
+        .addService(ProtoReflectionService.newInstance())
         .build();
 
     server.start();
